dc313ac34048aa7721c03e7306e54a495a14cd3c
amitiwary999 <tiwarya9@gmail.com>
2024-03-11 16:31:15 +0530

    manager init call

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
-	amqp "tskscheduler/task-scheduler/storage"
+	cnfg "tskscheduler/task-scheduler/config"
+	manag "tskscheduler/task-scheduler/scheduler"
+	storage "tskscheduler/task-scheduler/storage"
 
 	"github.com/joho/godotenv"
 )
@@ -13,20 +15,28 @@ func main() {
 	if err != nil {
 		fmt.Printf("error load env %v\n", err)
 	}
-	done := make(chan int)
 	rabbitmqUri := os.Getenv("RABBITMQ_URL")
 	exchange := os.Getenv("RABBITMQ_EXCHANGE")
 	exchangeType := os.Getenv("RABBITMQ_EXCHANGE_TYPE")
 	queue := os.Getenv("RABBITMQ_QUEUE")
 	exchangeKey := os.Getenv("RABBITMQ_EXCHANGE_KEY")
 	consumerTag := os.Getenv("RABBITMQ_CONSUMER_TAG")
-	consumer, err := amqp.NewConsumer(rabbitmqUri, exchange, exchangeType, queue, exchangeKey, consumerTag, done)
+	supabaseKey := os.Getenv("SUPABASE_KEY")
+	supabaseUrl := os.Getenv("SUPABASE_URL")
+
+	done := make(chan int)
+	consumer, err := storage.NewConsumer(rabbitmqUri, exchange, exchangeType, queue, exchangeKey, consumerTag, done)
 	if err != nil {
 		fmt.Printf("amq connection error %v\n", err)
 	} else {
 		consumer.SetupCloseHandler()
 	}
 
+	supa, error := storage.NewSupabaseClient(supabaseUrl, supabaseKey)
+	if error != nil {
+		fmt.Printf("supabase cloient failed %v\n", error)
+	}
+	manag.InitManager(consumer, supa, done, cnfg.LoadConfig())
 	<-done
 
 }
