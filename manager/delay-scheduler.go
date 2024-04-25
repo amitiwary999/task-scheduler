fa86d7383af03dae61ef9877f51924783115d604
amitiwary999 <tiwarya9@gmail.com>
2024-04-25 23:36:22 +0530

    queue to maintain schedule task according to execution order

---
+++ manager/delay-scheduler.go
@@ -0,0 +1,33 @@
+package manager
+
+import (
+	model "github.com/amitiwary999/task-scheduler/model"
+)
+
+type delayTask struct {
+	Task *model.Task
+	Time int64
+}
+
+type PriorityQueue []*delayTask
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Time < pq[j].Time
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(task interface{}) {
+	*pq = append(*pq, task.(*delayTask))
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	prev := *pq
+	task := prev[len(prev)-1]
+	*pq = prev[0 : len(prev)-1]
+	return task
+}
