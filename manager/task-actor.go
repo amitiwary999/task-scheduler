4e0d9081920ba6169f0297c0d01e36ab9a78f7ed
amitiwary999 <tiwarya9@gmail.com>
2024-06-16 22:43:44 +0530

    wip queue to process task

--- manager/task-actor.go
+++ manager/task-actor.go
@@ -1,7 +1,45 @@
 package manager
 
 type TaskActor struct {
-	workerCount uint16
-	done        chan int
-	taskQueue   chan struct{}
+	maxWorker uint16
+	done      chan int
+	taskChan  chan func()
+	taskQueue chan func()
+}
+
+func NewTaskActor(workerCount uint16, done chan int, tasksSize uint16) *TaskActor {
+	ta := &TaskActor{
+		maxWorker: workerCount,
+		done:      done,
+		taskQueue: make(chan func(), tasksSize),
+		taskChan:  make(chan func()),
+	}
+	go ta.Dispatch()
+	return ta
+}
+
+func (ta *TaskActor) SubmitTask(fn func()) {
+	ta.taskChan <- fn
+}
+
+func (ta *TaskActor) Dispatch() {
+	var workerCount uint16 = 0
+	for {
+		select {
+		case taskF, ok := <-ta.taskChan:
+			if !ok {
+				break
+			}
+			if workerCount < ta.maxWorker {
+				go ta.DoAction()
+			}
+			ta.taskQueue <- taskF
+		case <-ta.done:
+			return
+		}
+	}
+}
+
+func (ta *TaskActor) DoAction() {
+
 }
